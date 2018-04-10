301eb6632d18fc9ee2c99c52ee57d5cba48d6fdf
Vlad <irbis1307@yandex.ru>
2018-04-10 10:54:40 -0700

    add through association

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,8 @@
 class Airport < ApplicationRecord
   belongs_to :town
 
+  has_one :country, :through :town
+
   has_many :trips_from, class_name: "Trip", foreign_key: "airport_from_id"
   has_many :trips_to, class_name: "Trip", foreign_key: "airport_to_id"
 
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,7 @@
 class Country < ApplicationRecord
 	has_many :towns
 	has_many :comments, as: :commentable  
+	has_many :airports, through: :towns
 
 	validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
